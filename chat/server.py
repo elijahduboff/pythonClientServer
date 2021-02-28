a74e52a4a8eadf19245aa802ede405e3f29c9779
Elijah Duboff <elijahduboff@gmail.com>
2021-02-28 13:44:07 +0300

    finish homework 7

--- chat/server.py
+++ chat/server.py
@@ -1,7 +1,8 @@
-from socket import socket, AF_INET, SOCK_STREAM
+import select
 import json
 import argparse
 import logging
+from socket import socket, AF_INET, SOCK_STREAM
 import log.server_log_config
 from log.server_log_config import Log, log_func
 
@@ -48,6 +49,32 @@ def send_response_to_client(chat_client, server_json_response):
     # print(f'Ответ сервера отправлен клиенту {chat_client}')
 
 
+def read_client_message(read_list, chat_clients):
+    """ Функция принимает на вход списки клиентов чата, читает сообщения от клиентов чата и вовзращает словарь со всеми
+    сообщениями, отправленными в общий чат"""
+    messages = {}
+    for client in read_list:
+        try:
+            message = get_client_msg(client)
+            messages[client] = message
+        except:
+            logger.warning(f'Клиент {client} отключился от чата')
+            chat_clients.remove(client)
+
+    return messages
+
+
+def write_messages_to_client(messages, write_list, chat_clients):
+    for item in write_list:
+        for client, message in messages.items():
+            try:
+                send_response_to_client(item, message)
+            except:
+                logger.warning(f'Клиент {client} откллючился от чата')
+                client.close()
+                chat_clients.remove(client)
+
+
 # TODO Добавить проверку порта > 1024
 def main():
     parser = argparse.ArgumentParser(description='Chat Server Terminal\' Launcher')
@@ -58,13 +85,33 @@ def main():
     port = args.p
     sock = socket(AF_INET, SOCK_STREAM)
     sock.bind((addr, port))
-    sock.listen(1)
+    sock.listen(5)
+    sock.settimeout(2)
+    chat_clients = []
 
     while True:
-        chat_client, addr = sock.accept()
-        chat_client_msg = get_client_msg(chat_client)
-        server_response_msg = make_response_to_client(chat_client_msg)
-        send_response_to_client(chat_client, server_response_msg)
+        try:
+            chat_client, addr = sock.accept()
+        except OSError as e:
+            pass
+        else:
+            logger.info(f'Получен запрос на подключение от {chat_client}')
+            chat_clients.append(chat_client)
+        finally:
+            read_list = []
+            write_list = []
+            wait = 5
+            try:
+                read_list, write_list, error_list = select.select(chat_clients, chat_clients, [], wait)
+            except:
+                pass
+            messages = read_client_message(read_list, chat_clients)
+            if messages:
+                write_messages_to_client(messages, write_list, chat_clients)
+
+        # chat_client_msg = get_client_msg(chat_client)
+        # server_response_msg = make_response_to_client(chat_client_msg)
+        # send_response_to_client(chat_client, server_response_msg)
 
 
 if __name__ == '__main__':
